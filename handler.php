c08a40745cc37ec97c4849208bef99884f665aa0
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-11-01 14:00:21 +0000

    adding base code

--- handler.php
+++ handler.php
@@ -1,54 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-
-  <!-- Stylesheet Stuff goes here -->
-  <link rel="stylesheet" href="./css/styles.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-
-  <!-- Favicon Stuff goes here-->
+  <meta charset="UTF-8">
   <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
   <link rel="manifest" href="favicon/site.webmanifest">
-
-  <!-- Title Stuff goes here -->
-  <title>Julia Fan Page</title>
-
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Circle </title>
 </head>
 
 <body>
 
-  <!-- Always shows a header, even in smaller screens. -->
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <div cl<div> class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">Area and Circumference Calculator</span>
-        <!-- Add spacer, to align navigation to the right -->
+        <span class="mdl-layout-title">Circle Calculator</span>
         <div class="mdl-layout-spacer"></div>
       </div>
     </header>
     <main class="mdl-layout__content">
-      <div class="page-content"><br />
-
-        <!-- Your content goes here -->
+      <div class="page-content" style="text-align: center; margin-top: 50px;">
 
 
-        <center>
-
-
-          <?php
-          $myVariable = "";
-          if (isset($_POST['myVariable'])) {
-            $myVariable = $_POST['myVariable'];
-          }
-          echo "<h1>My Program</h1>\n";
-          echo "<p>My Variable is = " . $myVariable . "</p>\n";
-          ?>
+        <?php
+        $myVariable = "";
+        if (isset($_POST['myVariable'])) {
+          $myVariable = $_POST['myVariable'];
+        }
+        echo "<h1>My Program</h1>\n";
+        echo "<p>My Variable is = " . $myVariable . "</p>\n";
+        ?>
 
 </body>
 

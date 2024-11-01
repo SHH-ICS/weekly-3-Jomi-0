ae44fce3ccd5f056c80a7709db24c48fb2b31c23
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-11-01 14:32:05 +0000

    added favicon

--- assignment.py
+++ assignment.py
@@ -1,2 +1,16 @@
 import python
-input("")
+price = 0.0
+
+
+if toppings <=4:
+  if toppings == 1
+    price += 1
+  elif toppings == 2
+    price += 1.75
+  elif toppings == 3
+    price += 2.50
+  elif toppings == 4
+    price += 3.35
+
+
+

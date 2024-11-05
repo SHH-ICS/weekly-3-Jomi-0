e0b7ceefe7f96d6dfce78222ac225188822e305b
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-11-05 23:04:28 +0000

    finished weekly assignment 3

--- assignment.py
+++ assignment.py
@@ -1,16 +1,23 @@
-import python
+import sys
+
+
 price = 0.0
 
+pizzasize = sys.argv[1]
+toppings = int(sys.argv[2])
 
-if toppings <=4:
-  if toppings == 1
-    price += 1
-  elif toppings == 2
+if pizzasize == "L":
+    price += 6.0
+elif pizzasize == "XL":
+    price += 10.0
+if toppings == 1:
+    price += 1.0
+elif toppings == 2:
     price += 1.75
-  elif toppings == 3
+elif toppings == 3:
     price += 2.50
-  elif toppings == 4
+elif toppings == 4:
     price += 3.35
 
-
-
+# Output the final price
+print(price)
--- handler.php
+++ handler.php
@@ -3,80 +3,39 @@
 
 <head>
   <meta charset="UTF-8">
-  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-  <link rel="manifest" href="favicon/site.webmanifest">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <title>Pizza</title>
-  <!-- Uses a transparent header that draws on top of the layout's background -->
-  <style>
-    .demo-layout-transparent {
-      background: url('../assets/demos/transparent.jpg') center / cover;
-    }
-
-    .demo-layout-transparent .mdl-layout__header,
-    .demo-layout-transparent .mdl-layout__drawer-button {
-      /* This background is dark, so we set text to white. Use 87% black instead if
-     your background is light. */
-      color: white;
-    }
-  </style>
-
-  <div class="demo-layout-transparent mdl-layout mdl-js-layout">
-    <header class="mdl-layout__header mdl-layout__header--transparent">
-      <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">Title</span>
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-        <!-- Navigation -->
-        <nav class="mdl-navigation">
-          <a class="mdl-navigation__link" href="">Link</a>
-          <a class="mdl-navigation__link" href="">Link</a>
-          <a class="mdl-navigation__link" href="">Link</a>
-          <a class="mdl-navigation__link" href="">Link</a>
-        </nav>
-      </div>
-    </header>
-    <div class="mdl-layout__drawer">
-      <span class="mdl-layout-title">Title</span>
-      <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-    <main class="mdl-layout__content">
-    </main>
-  </div>
+  <title>Pizza Price Calculation</title>
 </head>
 
 <body>
-
-  <div cl<div> class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Circle Calculator</span>
+        <span class="mdl-layout-title">Pizza Price</span>
         <div class="mdl-layout-spacer"></div>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="page-content" style="text-align: center; margin-top: 50px;">
-
-
         <?php
-        $myVariable = "";
-        if (isset($_POST['myVariable'])) {
-          $myVariable = $_POST['myVariable'];
-        }
-        echo "<h1>My Program</h1>\n";
-        echo "<p>My Variable is = " . $myVariable . "</p>\n";
-        ?>
+        if (isset($_POST['pizzasize']) && isset($_POST['toppings'])) {
+          $pizzasize = escapeshellarg($_POST['pizzasize']);
+          $toppings = intval($_POST['toppings']);
 
+          $output = shell_exec("python3 assignment.py $pizzasize $toppings");
+
+          echo "<p>The total price of your pizza is: $" . htmlspecialchars($output) . "</p>";
+        } else {
+          echo "<p>Error: Please select a pizza size and the number of toppings.</p>";
+        }
+        ?> <a href="javascript:history.back()" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+          Go Back
+        </a>
+        <a href="Thankyou.html"class= "mdl-button mdl-js-button mdl-button--raised mdl-button--colored">Proceed</a>
+
+      </div>
+    </main>
+  </div>
 </body>
 
 </html>
